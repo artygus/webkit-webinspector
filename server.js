0a24b49c9ba42be1de2181a8e940b38f2a625a15
artygus <hero@engineeriam.net>
2015-10-03 12:28:02 +0300

    added options to serve different versions

--- server.js
+++ server.js
@@ -4,9 +4,23 @@ const connect = require('connect'),
       serveStatic = require('serve-static'),
       path = require('path');
 
-var staticPath = path.join(__dirname, 'lib', 'WebInspectorUI'),
-    port = process.argv.slice(2)[0];
+var version = 'latest',
+    port = 8080;
+
+for(var arg of process.argv.slice(2)) {
+  if (arg[0] === 'v') {
+    var argVer = arg.substr(1);
+
+    if (['7', '8'].indexOf(argVer) !== -1) {
+      version = arg;
+    }
+  } else {
+    port = arg;
+  }
+}
+
+const staticPath = path.join(__dirname, 'lib', 'WebInspectorUI', version);
 
 connect()
   .use(serveStatic(staticPath))
-  .listen(port || 8080);
+  .listen(port);
